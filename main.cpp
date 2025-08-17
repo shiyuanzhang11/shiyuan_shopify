4624160efc624cf5bc1a13b2f384c3cd372c33c5
shiyuanzhang11 <shiyuanzhang11@outlook.com>
2025-08-16 21:37:08 -0400

    shopify second

--- main.cpp
+++ main.cpp
@@ -6,9 +6,15 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace::std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello Shopify!\n";
+    vector<int> vt;
+    for(int i=0;i<100;i++)
+        vt.push_back(i);
     return 0;
 }
