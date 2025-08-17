598b53ecdbec92512e1eae558ff87ebf7f61fd00
shiyuanzhang11 <shiyuanzhang11@outlook.com>
2025-08-16 21:34:00 -0400

    shopify first time commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  shopify
+//
+//  Created by Regan Zhang on 8/15/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello Shopify!\n";
+    return 0;
+}
